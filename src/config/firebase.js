33b3aa84e2704790b48f64ddca22db0cd1190935
afiukwa john <johnafiukwa2011@gmail.com>
2024-11-04 14:25:32 +0100

    New commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbrs2E9y64nUVOsPQnmH9VdPI9WOFOJls",
-  authDomain: "jbsmr-users.firebaseapp.com",
-  projectId: "jbsmr-users",
-  storageBucket: "jbsmr-users.appspot.com",
-  messagingSenderId: "222018187006",
-  appId: "1:222018187006:web:4e68f1a3eb0aa7c4957faf",
-  measurementId: "G-E3SETMHE7F",
+  
 };
 
 const app = initializeApp(firebaseConfig);
