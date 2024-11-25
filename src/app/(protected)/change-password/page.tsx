c3836cc47dd4d508daf7c54b4d7f61ed6882c9c4
afiukwa john <johnafiukwa2011@gmail.com>
2024-11-25 20:52:08 +0100

    new changes to form submit and placeholders

--- src/app/(protected)/change-password/page.tsx
+++ src/app/(protected)/change-password/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import { FaCircleUser } from "react-icons/fa6";
 
 export default function ChangePassword() {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Password changed or not");
+  };
   return (
     <>
       <div className=" flex flex-col items-center md:mt-[5rem]">
@@ -10,27 +19,37 @@ export default function ChangePassword() {
           <FaCircleUser className="text-[6rem] md:text-[12rem] text-[#1b9c85]" />
 
           <div className="w-80">
-            <form>
-              <div className="profile-inputs-container">
+            <form onSubmit={handleSubmit}>
+              <div className="jbsmr-inputs-container pb-3">
                 <label>Current Password</label>
-                <input type="password" className="profile-inputs" />
+                <input
+                  type="password"
+                  className="jbsmr-inputs"
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
               </div>
-
-              <div className="profile-inputs-container">
+              <div className="jbsmr-inputs-container pb-3">
                 <label>New Password</label>
-                <input type="password" className="profile-inputs" />
+                <input
+                  type="password"
+                  className="jbsmr-inputs"
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </div>
+              <div className="jbsmr-inputs-container">
+                <label>Confirm New Password</label>
+                <input
+                  type="password"
+                  className="jbsmr-inputs"
+                  onChange={(e) => setConfirmNewPassword(e.target.value)}
+                />
               </div>
 
-              <div className="profile-inputs-container">
-                <label>Confirm New Password</label>
-                <input type="password" className="profile-inputs" />
-              </div>
+              <button className="primary-btns mt-10 p-3" type="submit">
+                Save Changes
+              </button>
             </form>
           </div>
-        </div>
-
-        <div>
-          <button className="global-btns">Save Changes</button>
         </div>
       </div>
     </>
--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -3,9 +3,24 @@
 import Link from "next/link";
 import { FaCircleUser } from "react-icons/fa6";
 import { useState } from "react";
+import { states } from "@src/contents/constants";
 
 export default function Profile() {
+  const [fName, setFname] = useState("");
+  const [lName, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+
   const [active, setActive] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Profile Submitted");
+    setActive(!active);
+  };
 
   return (
     <>
@@ -13,95 +28,117 @@ export default function Profile() {
         <FaCircleUser className="text-[10rem] text-[#1b9c85]" />
 
         <div className=" w-full p-3 flex flex-col float-right">
-          <div className="w-full flex items-center justify-between">
+          <div className="w-full flex items-center justify-between mt-5">
             <h3 className="font-bold text-lg text-[#0f172a]">My Profile</h3>
             <button
-              className="bg-[#1b9c85] text-white font-semibold p-2 px-5 rounded-lg
-              hover:bg-[#178e79] duration-300"
-              onClick={() => setActive(!active)}
+              className="primary-btns p-2 px-5"
+              onClick={() => setActive(true)}
+              disabled={active}
             >
-              {active ? "Done" : "Edit"}
+              Edit Profile
             </button>
           </div>
 
           <div className="mt-10">
-            <form>
-              <div className="double-inputs-container">
-                <div className="profile-inputs-container">
+            <form onSubmit={handleSubmit}>
+              <div className="md:flex items-center justify-between gap-10 md:pb-3">
+                <div className="jbsmr-inputs-container">
                   <label>First Name</label>
                   <input
                     type="text"
-                    className={`profile-inputs ${active ? "border" : ""}`}
+                    className={`jbsmr-inputs ${active ? "border" : ""}`}
+                    onChange={(e) => setFname(e.target.value)}
                     disabled={!active}
                   />
                 </div>
 
-                <div className="profile-inputs-container">
+                <div className="jbsmr-inputs-container">
                   <label>Last Name</label>
                   <input
                     type="text"
-                    className={`profile-inputs ${active ? "border" : ""}`}
+                    className={`jbsmr-inputs ${active ? "border" : ""}`}
+                    onChange={(e) => setLname(e.target.value)}
                     disabled={!active}
                   />
                 </div>
               </div>
 
-              <div className="single-inputs-container">
+              <div className="flex flex-col gap-3 pb-3">
                 <label>Email</label>
                 <input
                   type="email"
-                  className={`profile-inputs ${active ? "border" : ""}`}
+                  className={`jbsmr-inputs ${active ? "border" : ""}`}
+                  onChange={(e) => setEmail(e.target.value)}
                   disabled={!active}
                 />
               </div>
 
-              <div className="single-inputs-container">
+              <div className="flex flex-col gap-3 pb-3">
                 <label>Address</label>
                 <input
                   type="text"
-                  className={`profile-inputs ${active ? "border" : ""}`}
+                  className={`jbsmr-inputs ${active ? "border" : ""}`}
+                  onChange={(e) => setAddress(e.target.value)}
                   disabled={!active}
                 />
               </div>
 
-              <div className="single-inputs-container">
+              <div className="flex flex-col gap-3 pb-3">
                 <label>Mobile Number</label>
                 <input
                   type="number"
-                  className={`profile-inputs ${active ? "border" : ""}`}
+                  className={`jbsmr-inputs ${active ? "border" : ""}`}
+                  onChange={(e) => setMobileNumber(e.target.value)}
                   disabled={!active}
                 />
               </div>
 
-              <div className="double-inputs-container">
-                <div className="profile-inputs-container">
+              <div className="md:flex items-center justify-between gap-10 md:pb-3">
+                <div className="jbsmr-inputs-container">
                   <label>City</label>
                   <select
-                    className={`profile-inputs ${active ? "border" : ""}`}
+                    className={`jbsmr-inputs ${active ? "border" : ""}`}
+                    onChange={(e) => setCity(e.target.value)}
                     disabled={!active}
                   >
                     {/* cities options array map */}
                   </select>
                 </div>
 
-                <div className="profile-inputs-container">
+                <div className="jbsmr-inputs-container">
                   <label>State</label>
                   <select
-                    className={`profile-inputs ${active ? "border" : ""}`}
+                    className={`jbsmr-inputs ${active ? "border" : ""}`}
+                    onChange={(e) => setState(e.target.value)}
                     disabled={!active}
                   >
-                    {/* states options array map */}
+                    {states.map((item, index) => {
+                      return <option key={index}>{item}</option>;
+                    })}
                   </select>
                 </div>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <button
+                  className="primary-btns mt-10 p-3"
+                  type="submit"
+                  hidden={!active}
+                >
+                  Save Changes
+                </button>
               </div>
             </form>
           </div>
         </div>
-
-        <Link href={"/change-password"} className="global-btns">
-          Change Password
-        </Link>
       </div>
+
+      <Link
+        href={"/change-password"}
+        className="primary-btns mt-10 p-3 float-right mr-4"
+      >
+        Change Password
+      </Link>
     </>
   );
 }
